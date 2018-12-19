f9efe97d50659479d4285da8ef71afe248bd6848
Giuseppe Digilio <giuseppe.digilio@4science.it>
2018-12-19 15:21:02 +0100

    [CST-477] Show reject reason in rejected item list

--- dspace-api/src/main/java/org/dspace/discovery/WorkspaceitemSolrServiceIndexPlugin.java
+++ dspace-api/src/main/java/org/dspace/discovery/WorkspaceitemSolrServiceIndexPlugin.java
@@ -7,19 +7,27 @@
  */
 package org.dspace.discovery;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
 import javax.persistence.Transient;
 
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.apache.solr.common.SolrInputDocument;
+import org.dspace.authorize.AuthorizeException;
 import org.dspace.content.Bitstream;
 import org.dspace.content.Bundle;
 import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
+import org.dspace.content.service.BitstreamService;
 import org.dspace.core.Context;
 import org.dspace.discovery.configuration.DiscoverySearchFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Add additional fields and values to Solr index:
@@ -32,6 +40,7 @@ public class WorkspaceitemSolrServiceIndexPlugin implements SolrServiceIndexPlug
 	private static final String SOLR_FIELD_NAME_FOR_REJECTED = "rejected";
 	private static final String SOLR_FIELD_NAME_FOR_REJECTED_DATE = "rejecteddate";
 	private static final String SOLR_FIELD_NAME_FOR_REJECTED_USER = "rejecteduser";
+	private static final String SOLR_FIELD_NAME_FOR_REJECTED_REASON = "rejectedreason";
 	
 	private static final String BUNDLE_NAME = "MESSAGE";
 	private static final String BITSTREAM_SUBMISSION_REJECTED = "Submission Rejected";
@@ -42,6 +51,9 @@ public class WorkspaceitemSolrServiceIndexPlugin implements SolrServiceIndexPlug
     @Transient
     private static Logger log = Logger.getLogger(WorkspaceitemSolrServiceIndexPlugin.class);
     
+    @Autowired(required = true)
+    private BitstreamService bitstreamService;
+
     @Override
     public void additionalIndex(Context context, DSpaceObject dso, SolrInputDocument document,
             Map<String, List<DiscoverySearchFilter>> searchFilters)
@@ -86,6 +98,14 @@ public class WorkspaceitemSolrServiceIndexPlugin implements SolrServiceIndexPlug
 	                                		+ bitstream.getMetadata(BITSTREAM_CREATOR));
 	                                document.addField(SOLR_FIELD_NAME_FOR_REJECTED_USER, 
 	                                		bitstream.getMetadata(BITSTREAM_CREATOR));
+	                                
+	                                String content = retrieveBitstreamContent(context, bitstream);
+	                                log.debug("Adding field: " + SOLR_FIELD_NAME_FOR_REJECTED_REASON + ": "
+	                                		+ content);
+	                                document.addField(SOLR_FIELD_NAME_FOR_REJECTED_REASON, 
+	                                		content);
+	                                
+
                             	}
                             }
                         }
@@ -93,5 +113,25 @@ public class WorkspaceitemSolrServiceIndexPlugin implements SolrServiceIndexPlug
                 }
             }
         }
+    }
+    
+    private String retrieveBitstreamContent(Context context, Bitstream bitstream) {
+    	InputStream inputStream = null;
+    	String content = "";
+    	
+    	try {
+    		inputStream = bitstreamService.retrieve(context, bitstream);
+        	content = IOUtils.toString(inputStream, Charsets.UTF_8); 
+			inputStream.close();
+		} catch (IOException | SQLException | AuthorizeException e) {
+			try {
+				inputStream.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+    	
+		return content;
     }
 }
